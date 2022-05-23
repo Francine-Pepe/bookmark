51a422448fe4603ab67cca58b61c128541d21ad7
Francine <franmelopepe2@gmail.com>
2022-05-23 12:54:32 +0200

    navbarmobile created

---
+++ src/Components/MainContent/MainContentTop.js
@@ -0,0 +1,15 @@
+import "./MainContent.css";
+import { Container, VStack } from "@chakra-ui/react";
+
+export default function MainContentTop() {
+  return (
+    <>
+      <VStack>
+        
+        <Container maxW="100vw" bg="green" color="#262626">
+          "container.sm" Container
+        </Container>
+      </VStack>
+    </>
+  );
+}
